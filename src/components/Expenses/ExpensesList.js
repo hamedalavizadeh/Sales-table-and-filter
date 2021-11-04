65d30178178b13b69d9ef62e982a311642f33887
Hamed Alavi <hamedalavi2468@gmail.com>
2021-11-04 22:37:48 +0330

    filter

---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+import './ExpensesList.css';
+
+const ExpensesList = (props) => {
+
+    let expensesContent = <p>No Expense Found.</p>
+
+    if (props.item.length === 0) {
+        return <h2 className="expensen-list__fallback">Found no expenses.</h2>
+    }
+
+    return (
+        <ul className="expenses-list">
+            {
+                props.item.map((expense) => (
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                ))
+            }
+        </ul>
+    )
+}
+
+export default ExpensesList;
